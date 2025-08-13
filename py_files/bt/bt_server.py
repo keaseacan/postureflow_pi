6f27d9090b309d20dafa374c71438f15b1384e4c
keaseacan <keaboy2308@gmail.com>
2025-08-14 02:30:47 +0800

    disabled publish uuid

--- py_files/bt/bt_server.py
+++ py_files/bt/bt_server.py
@@ -34,7 +34,7 @@ NUS_RX_UUID      = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'
 NUS_TX_UUID      = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'
 
 # --- ADV privacy toggle ---
-ADVERTISE_SERVICE_UUIDS = True  # True = include UUIDs in ADV; False = omit (stealth scan-by-name)
+ADVERTISE_SERVICE_UUIDS = False  # True = include UUIDs in ADV; False = omit (stealth scan-by-name)
 
 # ---------- BlueZ constants ----------
 BLUEZ_SERVICE_NAME = 'org.bluez'
